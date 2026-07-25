08585d2804f40f0e8959811ff0059d8fb9797a5e
Lazy Sloth <niteshpatelsd@proton.me>
2026-07-26 01:47:40 +0530

    removed main  srource code

--- src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "../include/logger/config.hpp"
-#include <cstdlib>
-#include <fmt/color.h>
-
-int main() {
-  fmt::println("{}", static_cast<bool>(logger::LoggerMode::Async));
-  fmt::println("{}", static_cast<bool>(logger::LoggerMode::Sync));
-  return EXIT_SUCCESS;
-}
